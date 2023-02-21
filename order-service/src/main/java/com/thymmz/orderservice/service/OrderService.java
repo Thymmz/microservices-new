1dbf011a51aa1307e0beecb34daa99d29bd671d7
Thymmz <atharva.deshpande228@gmail.com>
2023-02-21 11:43:24 -0600

    orderService updated for List<skuCodes>

--- order-service/src/main/java/com/thymmz/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/thymmz/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.thymmz.orderservice.service;
 
+import com.thymmz.orderservice.dto.InventoryResponse;
 import com.thymmz.orderservice.dto.OrderLineItemsDto;
 import com.thymmz.orderservice.dto.OrderRequest;
 import com.thymmz.orderservice.model.Order;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,13 +36,22 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        Boolean result = webClient.get()
-                .uri("http://localhost:8082/api/inventory")
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call InventoryService and place order,  if product is in stock
+
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
-                .bodyToMono(Boolean.class)
+                .bodyToMono(InventoryResponse[].class)
                 .block();
 
-        if (result){
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock){
             orderRepository.save(order);
         }
         else {
